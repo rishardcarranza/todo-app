dfe963da415809b57bfd70e5662d7bb66e1a3b44
Richard Carranza <rishardcarranza@gmail.com>
2022-07-18 15:49:46 -0600

    High order components example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
-// import reportWebVitals from './reportWebVitals';
+// import { App } from './App';
 import './index.css';
+
+function App(props) {
+    return (
+        <h1>{props.saludo}, {props.nombre}!</h1>
+    );
+}
+
+function withSaludo(WrappedComponent) {
+    return function WrappedComponentWithSaludo(saludo) {
+        return function ComponentTrue(props) {
+            return (
+                <React.Fragment>
+                    <WrappedComponent {...props} saludo={saludo}/>
+                    <p>Estamos acompañando al wrapped component</p>
+                </React.Fragment>
+            )
+        }
+    }
+}
+
+const AppWithSaludo = withSaludo(App)("Buenas noches");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    <AppWithSaludo nombre="Cesarito" />
+    // <App saludo="Buenas" nombre="Rich" />
+    // <App/>
 );
